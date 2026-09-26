839e5d52799fe1b87b528a15571a125a6ab9fb02
Spot My Business - Admin <smb.spot.my.business@gmail.com>
2026-09-26 12:35:48 +0530

    Update page.tsx

--- src/app/bike-shifting/page.tsx
+++ src/app/bike-shifting/page.tsx
@@ -218,7 +218,7 @@ export default function BikeShiftingPage() {
         /* Hero Visual Image: Drop your image in /public/images/ and set src="/images/your-image.png" */
         image={
           <img
-            src="/images/bike_transportation.jpeg"
+            src="/images/packaging.png"
             alt="Bike Transport Two Wheeler Shifting Services - Bharat Relocators"
           />
         }
